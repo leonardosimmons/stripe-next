ea5ed31670be6180b0061bc324b6d1a69b3b0dda
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-07-19 19:06:51 -0700

    updated for base config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,11 @@
 import React from 'react';
 
 
-function Index(): JSX.Element {
+import BaseStripe from '../base/index';
+
+const Index: React.FunctionComponent = (): JSX.Element => {
   return (
-    <h1>{'Stripe-Next Payment Module'}</h1>
+    <BaseStripe />
   );
 };
 
