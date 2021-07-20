13bf55b2c2353c6274dedbca9c890b6d42d7d2a8
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-07-20 14:29:00 -0700

    added

---
+++ src/components/CreditCardForm.tsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+import { CardElement } from "@stripe/react-stripe-js";
+
+
+const StripeCheckout: React.FunctionComponent = (): JSX.Element => {
+  return (
+    <CardElement />
+  );
+};
+
+export default StripeCheckout;
