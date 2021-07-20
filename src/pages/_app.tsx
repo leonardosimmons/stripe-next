11f2822f6e22f2393770cd88036b4db64e95808e
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-07-20 14:29:27 -0700

    updated for main version

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 
 
 import '../styles';
+import '../styles/css/global.css';
+import '../styles/css/normalize.css';
 import type { AppProps } from 'next/app';
 
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 };
-
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,36 @@
 
 import React from 'react';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe, Stripe } from '@stripe/stripe-js';
+
+import CreditCardForm from '../components/CreditCardForm';
 
 
-import BaseStripe from '../base/index';
+const stripePromise: Promise<Stripe | null> = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string);
 
 const Index: React.FunctionComponent = (): JSX.Element => {
   return (
-    <BaseStripe />
+    <div className="sr-root">
+      <div className="sr-main">
+        <form id="payment-form" className="sr-payment-form">
+          <div className="sr-combo-inputs-row">
+            <div className="sr-input sr-card-element" id="card-element">
+            <Elements stripe={stripePromise}>
+              <CreditCardForm />
+            </Elements>
+            </div>
+          </div>
+          <div className="sr-field-error" id="card-errors" role="alert"></div>
+          <button id="submit">
+            <div className="spinner hidden" id="spinner"></div>
+            <span id="button-text">Pay</span><span id="order-amount"></span>
+          </button>
+        </form>
+        <div className="sr-result hidden">
+          <p>Payment completed<br /></p>
+        </div>
+      </div>
+    </div>
   );
 };
 
