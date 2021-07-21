1373cb72a95f9b88ff33d66b7cb8109c2d4fcd9e
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-07-21 11:49:38 -0700

    made stripe global -> fraud protection

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,21 @@ import '../styles';
 import '../styles/css/global.css';
 import '../styles/css/normalize.css';
 import type { AppProps } from 'next/app';
+import { loadStripe, Stripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import { getStripeKey } from '../helpers/config';
 
+const stripePromise: Promise<Stripe | null> = getStripeKey().then((key: string | void) => loadStripe(key as string));
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <>
+  {
+    stripePromise &&
+    <Elements stripe={stripePromise}>    
+      <Component {...pageProps} />
+    </Elements>
+  }
+  </>
+  )
 };
