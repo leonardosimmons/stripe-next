d6b7b6f31aaad9f074a69177aea1b5146d5e389a
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-07-28 23:49:00 -0700

    added back styles

--- src/components/PaymentForm.tsx
+++ src/components/PaymentForm.tsx
@@ -7,67 +7,63 @@ import { formatAmount } from "../helpers/functions";
 import { AmountToken } from "../utils/types/custom/types";
 
 
-type Props = {
-  config?: any;
-}
-
 const testToken: AmountToken = {
   amount: 450.45,
   quantity: 1,
   currency: 'usd'
 };
 
-const StripeCheckout: React.FunctionComponent<Props> = ({ config }): JSX.Element => {  
-  const stripe = useStripeController();
+const StripeCheckout: React.FunctionComponent = (): JSX.Element => {  
+  const { errorCheck, form, status, submit } = useStripeController();
 
   return (
     <div className="sr-main">
       <form 
         id="payment-form" 
         className="sr-payment-form" 
-        onSubmit={stripe.submit}
-        ref={stripe.form.styles.wrapper}
+        onSubmit={submit}
+        ref={form.styles.wrapper}
       >
         <div className="sr-combo-inputs-row">
           <div id="card-element" className="sr-input sr-card-element">
-            <CardElement onChange={stripe.errorCheck}/>
+            <CardElement onChange={errorCheck}/>
           </div>
         </div>
         <div 
           id="card-errors" 
           className="sr-field-error" 
           role="alert"
-          ref={stripe.form.styles.error}>
-        { stripe.status.error 
-          ? stripe.status.error
+          ref={form.styles.error}>
+        { status.error 
+          ? status.error
           : null  
         }    
         </div>
         <button 
           id="submit" 
-          ref={stripe.form.styles.button}
-          disabled={stripe.status.disabled || stripe.status.processing || stripe.status.succeeded}>
+          ref={form.styles.button}
+          disabled={status.disabled || status.processing || status.succeeded}>
           <div 
             id="spinner" 
             className="spinner hidden" 
-            ref={stripe.form.styles.spinner}/>
+            ref={form.styles.spinner}/>
           {
-            !stripe.status.succeeded && !stripe.status.processing
+            !status.succeeded && !status.processing
             ? <>  
                 <span 
                 id="button-text" 
-                ref={stripe.form.styles.text}>{'Pay'}</span>
+                ref={form.styles.text}>{'Pay'}</span>
                 <span id="order-amount">{` ${formatAmount(testToken)}`}</span>
               </>
-            : stripe.status.processing
-              ? <span id="button-text" ref={stripe.form.styles.text}></span>
-              : <span id="button-text" ref={stripe.form.styles.text}></span>
+            : status.processing
+              ? <span id="button-text" ref={form.styles.text}></span>
+              : <span id="button-text" ref={form.styles.text}></span>
           }
         </button>
       </form>
-      <div className="sr-result hidden" ref={stripe.form.styles.result}>
+      <div className="sr-result hidden" ref={form.styles.result}>
         <p style={{textAlign: 'center', fontWeight: 'bold'}}>{'Payment completed'}<br /></p>
-        <pre ref={stripe.form.styles.pre}>
+        <pre ref={form.styles.pre}>
           <iframe src="https://giphy.com/embed/l41lS0IgRIFkAuA5G" width="280" height="280" frameBorder="0" className="giphy-embed" style={{margin: '0 auto'}} allowFullScreen></iframe><p><a href="https://giphy.com/gifs/dancing-friday-weekend-l41lS0IgRIFkAuA5G"></a></p>
         </pre>
       </div>
