047917a38eb03977f87aa170ad336f5a52bd66db
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-08-13 00:21:18 -0700

    added layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,19 @@
 
 import React from 'react';
 
+import layoutStyles from '../containers/layout/Layout.module.scss';
+
+import Layout from '../containers/layout/layout';
 import PaymentForm from '../components/payment-form/PaymentForm';
 
 
 const Index: React.FunctionComponent = (): JSX.Element => {
   return (
-    <div className="root">
+    <Layout
+      title={'Home | Stripe - Next.js Integration'}
+      styles={layoutStyles}>
       <PaymentForm />
-    </div>
+    </Layout>
   );
 };
 
