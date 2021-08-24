5a97824e2644ff5500644933107a2ed8816b6e09
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-08-24 16:55:18 -0700

    split app into smaller components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,19 +6,15 @@ import { Product, ProductCard as ProductCardType } from '../utils/types/custom/t
 
 import styles from '../containers/pages/index/Index.module.scss';
 import layoutStyles from '../containers/layout/Layout.module.scss';
-import productStyles from '../components/card/ProductCard.module.scss';
 
-import { Context } from '../context/DemoContext';
 import { StatusContext } from '../context/StatusContext';
 
 import Layout from '../containers/layout/layout';
-import PaymentForm from '../components/payment-form/PaymentForm';
 import Container from '../components/base/Container';
-import Heading from '../components/base/Heading';
-import Grid from '../components/grid/Grid';
-import ProductCard from '../components/card/ProductCard';
-import Toggle from '../components/toggle/ToggleSwitch';
-import ShippingForm from '../components/shipping/Shipping';
+import ShippingForm from '../containers/pages/index/stages/shipping/Shipping';
+import PaymentForm from '../containers/pages/index/stages/payment/Payment';
+import Intro from '../containers/pages/index/stages/intro/Intro';
+import ProductSelection from '../containers/pages/index/stages/selection/Selection';
 
 
 const {
@@ -67,70 +63,7 @@ export const getStaticProps: GetStaticProps = async () => {
 
 
 function Index({ cards, products }: InferGetStaticPropsType<typeof getStaticProps>): JSX.Element {
-  const context = React.useContext(Context);
   const status = React.useContext(StatusContext);
-
-  function startDemo(): void {
-    status.dispatch({ type: 'SET_PENDING', stage: 'selection' });
-  };
-
-  //* Payment type toggle
-  function handlePaymentType(e: React.ChangeEvent<HTMLInputElement>): void {
-    if (e.target.checked) {
-      context.dispatch({ type: 'SET_PAYMENT_TYPE', payment: "monthly"});
-      return;
-    }
-    context.dispatch({ type: 'SET_PAYMENT_TYPE', payment: "once"});
-  };
-
-  //* Prodcut Selection Form handling
-  function toggleChecked(index: number, checked: boolean): void {
-    if (checked) {
-      document.querySelector<HTMLInputElement>(`#card-${index+1}`)!.checked = true;
-      return;
-    }
-    document.querySelector<HTMLInputElement>(`#card-${index+1}`)!.checked = false;
-  };
-
-  function handleSelected(e: React.ChangeEvent<HTMLInputElement>): void {
-    let prods: Array<number> = [];
-    const prod_id: number = parseInt(e.target.value);
-
-    if (e.target.checked) {
-      prods = context.state.selectedProducts;
-      if (!prods.includes(prod_id)) {
-        prods.push(prod_id);
-        context.dispatch({ type: 'SET_SELECTED_PRODUCTS', products: prods });
-        handleTotal(prods);
-        return;
-      }
-    }
-    prods = context.state.selectedProducts.filter((product: number) => product !== prod_id);
-    context.dispatch({ type: 'SET_SELECTED_PRODUCTS', products: prods });
-    
-    if (context.state.selectedProducts.length === 0) {
-      context.dispatch({ type: 'SET_TOTAL', total: 0});
-      return;
-    }
-    handleTotal(prods);
-  };
-
-  function handleSubmit(e: React.FormEvent): void {
-    e.preventDefault();
-    status.dispatch({ type: 'SET_PENDING', stage: 'shipping' });
-  };
-
-  function handleTotal(prods: Array<number>): void {
-    let buffer: number = 0;
-    prods.map((prod: number) => {
-      products.map((product: Product) => {
-        if (product.meta.id === prod) {
-          buffer = buffer + product.details.price;
-        }
-      });
-    });
-    context.dispatch({ type: 'SET_TOTAL', total: buffer});
-  };
 
   return (
     <Layout
@@ -140,72 +73,10 @@ function Index({ cards, products }: InferGetStaticPropsType<typeof getStaticProp
     >
       <Container type="wrapper" styles={styles}>
         {status.state.stage === 'start' &&
-          <Heading 
-            type="main" 
-            styles={styles}
-            body={
-              <div className={styles.headingBtnBox}>
-                <button 
-                  className={styles.headingBtn} 
-                  onClick={() => startDemo()}>
-                    {'Start'}
-                </button>
-              </div>
-            }>
-            <span>{'Stripe/Next.js'}</span>
-            <span>{'Integration Demo'}</span>
-          </Heading>
+          <Intro />
         }
         {status.state.stage === 'selection' &&
-          <Container type="container" styles={styles}>
-            <Heading 
-              type="sub" 
-              styles={styles}
-              body={
-                <div className={styles.toggleBox}>
-                  <Toggle onChange={handlePaymentType} classes={styles.toggle}/>
-                </div>}>
-              {context.state.paymentType === "once"
-              ? "ONE-TIME"
-              : "MONTHLY"}
-            </Heading>
-            <form className={`${styles.form} relative`} onSubmit={handleSubmit}>
-              <Grid even grid={styles.grid}>
-                {cards.map((card: ProductCardType, index: number) => (
-                  <div className={styles.cardBox} key={index}>
-                    <label htmlFor={`card-${index+1}`} onClick={() => toggleChecked(index, card.checked)}>
-                      <ProductCard 
-                        fill
-                        priority
-                        card={card}
-                        styles={productStyles} 
-                        paymentType={context.state.paymentType}
-                      />
-                    </label>
-                    <input 
-                      type="checkbox" 
-                      value={card.id}
-                      id={`card-${index+1}`} 
-                      name={`card-${index+1}`} 
-                      checked={card.checked}
-                      onChange={handleSelected}
-                      onClick={() => card.checked = !card.checked}
-                    />
-                  </div>))}
-              </Grid>
-              <p>{`TOTAL: $${context.state.paymentType === "once"
-                            ? context.state.total
-                            : ((context.state.total + (context.state.total * .15)) / 12).toFixed(2) + '/month'}`}
-                {context.state.paymentType === "monthly"
-                ? <span>* For 12 months</span>
-                : <span>&nbsp;</span>}
-              </p>
-              <input 
-                type="submit" 
-                className={`${styles.btn} btn-activeFocus btn-hoverConfig`}
-              />
-            </form>
-          </Container>
+          <ProductSelection cards={cards} products={products}/>
         }
         {status.state.stage === "shipping" &&
           <ShippingForm />
