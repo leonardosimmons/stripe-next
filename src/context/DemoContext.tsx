46ffa9505079544ab4b73028abb521410745e97b
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-08-23 22:06:00 -0700

    updated for shipping

--- src/context/DemoContext.tsx
+++ src/context/DemoContext.tsx
@@ -6,12 +6,22 @@ import { ContextProps, DemoContext, PaymentType } from '../utils/types/custom/ty
 type DemoActions =
 | { type: 'RESET_CONTEXT' }
 | { type: 'SET_PAYMENT_TYPE', payment: PaymentType }
-| { type: 'SET_SELECTED_PRODUCTS', products: Array<number>}
+| { type: 'SET_SELECTED_PRODUCTS', products: Array<number> }
+| { type: 'SET_ADDRESS', address: string }
+| { type: 'SET_CITY', city: string }
+| { type: 'SET_POSTAL', postal: string }
+| { type: 'SET_STATE', state: string }
 | { type: 'SET_TOTAL', total: number };
 
 const initialState: DemoContext = {
   paymentType: "once",
   selectedProducts: [],
+  shipping: {
+    address: '',
+    city: '',
+    postal: '',
+    state: ''
+  },
   total: 0,
 };
 
@@ -20,11 +30,19 @@ function reducer(state: DemoContext, action: DemoActions): DemoContext {
     case 'RESET_CONTEXT':
       return initialState;
     case 'SET_PAYMENT_TYPE':
-      return {...state, paymentType: action.payment }
+      return {...state, paymentType: action.payment };
     case 'SET_SELECTED_PRODUCTS':
-      return {...state, selectedProducts: action.products}
+      return {...state, selectedProducts: action.products};
+    case 'SET_ADDRESS':
+      return {...state, shipping: {...state.shipping, address: action.address}};
+    case 'SET_CITY':
+      return {...state, shipping: {...state.shipping, city: action.city}};
+    case 'SET_POSTAL': 
+      return {...state, shipping: {...state.shipping, postal: action.postal}};
+    case 'SET_STATE':
+      return {...state, shipping: {...state.shipping, state: action.state}};
     case 'SET_TOTAL':
-      return {...state, total: action.total}
+      return {...state, total: action.total};
     default:
       return state;
   };
